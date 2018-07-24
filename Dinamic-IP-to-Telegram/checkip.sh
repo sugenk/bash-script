bd64079f1f92c0fa26859f500d8b98f41b90979d
Sugeng Kurniawan <sugeng.kurniawan@yahoo.com.sg>
2018-07-24 21:39:46 +0700

    Update checkip.sh

--- Dinamic-IP-to-Telegram/checkip.sh
+++ Dinamic-IP-to-Telegram/checkip.sh
@@ -4,19 +4,20 @@ Token="1234567890:ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 ChatID="1234567890"
 LogIP="/tmp/public-ip.txt"
 
+if [ ! -e "$LogIP" ] ; then
+    touch "$LogIP"
+fi
+
+if [ ! -w "$LogIP" ] ; then
+    echo cannot write to $LogIP
+    exit 1
+fi
+
 IPOld=$(<$LogIP)
 IPNew=$(curl -s https://ifconfig.co)
 
-#if [ "$IPOld" = "$IPNew" ]; then
-#  MSG="Your dinamic IP is not changed $IPOld = $IPNew"
-#else
-#  MSG="Your dinamic IP is changed to $IPNew"
-#  echo $IPNew > $LogIP
-#fi
-
 if [ "$IPOld" != "$IPNew" ]; then
   MSG="Your dinamic IP is changed to $IPNew"
+  curl -s -X POST "https://api.telegram.org/bot$Token/sendMessage" -d "chat_id=$ChatID&text=$MSG"
   echo $IPNew > $LogIP
 fi
-
-curl -s -X POST "https://api.telegram.org/bot$Token/sendMessage" -d "chat_id=$ChatID&text=$MSG"
